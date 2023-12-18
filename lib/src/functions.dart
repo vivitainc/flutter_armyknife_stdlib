e5c98298b3ffdc2622be7fbf636e30bd1a34f460
eaglesakura <eagle.sakura@gmail.com>
2023-12-18 12:17:01 +0900

    fix: nop()制御をもとに戻した refs #3

--- lib/src/functions.dart
+++ lib/src/functions.dart
@@ -3,7 +3,11 @@ Future delayed(final Duration duration) => Future<void>.delayed(duration);
 
 /// NOP命令（No Operation)を発行する.
 /// この命令はDart実行ループの1イテレーションをSkipしたい場合等に使用する.
-Future nop() => Future.value(null);
+///
+/// NOTE.
+/// Future.value()やFuture.microtask()を使用すると、Queueの最後に積まれないため、
+/// 確実にQueueの最後に積まれるFuture.delayed()を使用する.
+Future nop() => delayed(Duration.zero);
 
 /// 指定した2つの型が同一であればtrue.
 /// これはGenericsのTypeチェックに利用する.
